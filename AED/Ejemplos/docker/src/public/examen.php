74f766b4f2196823e4bceeaae8246f441af6b0cd
eduardomoralesglez <eduardomoralez24@gmail.com>
2025-09-19 16:21:50 +0100

    a

--- AED/Ejemplos/docker/src/public/examen.php
+++ AED/Ejemplos/docker/src/public/examen.php
@@ -1,3 +1,10 @@
 <?php 
-
+    declare( strict_type=1);
+    function esCapicua(int $n) {
+        $arrayInt = $n.str_split(1);
+        foreach ($arrayInt as $key => $value) {
+            echo $value;
+        }
+    }
+    echo esCapicua(123321);
 ?>
