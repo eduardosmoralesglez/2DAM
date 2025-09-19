2d97d4edd0b8a69fd407cf90e24dfd2c6c4b219a
eduardomoralesglez <eduardomoralez24@gmail.com>
2025-09-19 15:57:56 +0100

    Se crea tarea 2 AED

--- AED/Ejemplos/docker/src/public/examen.php
+++ AED/Ejemplos/docker/src/public/examen.php
@@ -1,7 +1,3 @@
 <?php 
-$foo = 'Bob'; // Asigna el valor 'Bob' a $foo 
-$bar = &$foo; // Referencia $foo vía $bar. 
-$bar = "Mi nombre es $bar"; // Modifica $bar... 
-echo $foo; // $foo también se modifica. 
-echo $bar; 
+
 ?>
