fe3c0373d5522df0b70cb12e4df05122cd64cd58
eduardomoralesglez <eduardomoralez24@gmail.com>
2025-09-19 15:46:36 +0100

    Se actualiza ejemplos

---
+++ AED/Ejemplos/docker/src/public/examen.php
@@ -0,0 +1,7 @@
+<?php 
+$foo = 'Bob'; // Asigna el valor 'Bob' a $foo 
+$bar = &$foo; // Referencia $foo vía $bar. 
+$bar = "Mi nombre es $bar"; // Modifica $bar... 
+echo $foo; // $foo también se modifica. 
+echo $bar; 
+?>
