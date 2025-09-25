f9508f03f625511e2d81f0b035518ed74019316e
eduardomoralesglez <eduardomoralez24@gmail.com>
2025-09-25 17:49:40 +0100

    Tarea AED progreso

--- AED/unidad1/tarea3/ejercicio01.php
+++ AED/unidad1/tarea3/ejercicio01.php
@@ -1,3 +1,11 @@
 <?php
-    
+
+$file = fopen("files/datos.txt", "r");
+fwrite($file, "Hola mundo");
+fclose($file);
+
+// Comprobar existencia
+if (file_exists("TestFolder/test.txt")) {
+    echo "El archivo existe\n";
+}
 ?>
