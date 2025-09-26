3f6064552b733e14cb087681c4123e34e256ac02
eduardomoralesglez <eduardomoralez24@gmail.com>
2025-09-26 18:47:03 +0100

    Se avanza en la tarea de la Calculadora PGL

--- PGL/unidad1/tareaCalculadora/calculadora/app/(tabs)/index.tsx
+++ PGL/unidad1/tareaCalculadora/calculadora/app/(tabs)/index.tsx
@@ -1,13 +1,36 @@
-import { Link } from 'expo-router';
-import { Text, View,  StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import ImageViewer from '@/components/ImagesViewer';
+import Button from '@/components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
+
 
 export default function Index() {
+    const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+    const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About Screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label='Choose a photo' onPress={pickImageAsync}></Button>
+        <Button label='Use this photo'></Button>
+      </View>
     </View>
   );
 }
@@ -17,15 +40,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
-
+    footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
 });
