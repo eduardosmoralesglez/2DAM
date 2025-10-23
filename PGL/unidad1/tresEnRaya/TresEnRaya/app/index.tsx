c12e584c6e08e57827323ff8a777f4f8842f744d
eduardomoralesglez <eduardomoralez24@gmail.com>
2025-10-23 19:36:24 +0100

    Se avanza en el proyecto tres en raya

--- PGL/unidad1/tresEnRaya/TresEnRaya/app/index.tsx
+++ PGL/unidad1/tresEnRaya/TresEnRaya/app/index.tsx
@@ -1,4 +1,30 @@
+import {  TouchableOpacity, Text } from "react-native";
+//TODO: Ver el tutorial: https://www.youtube.com/watch?v=U23lNFm_J70
+type SquarePromps = {
+  val: String
+}
 
-export default function Square() {
-  return <button className="square">X</button>
+
+export function Val(val: SquarePromps) {
+  return val;
+}
+export function Square() {
+  return (
+      <TouchableOpacity>
+        <Text>{}</Text>
+      </TouchableOpacity>
+  );
+}
+
+export function Board() {
+  return (
+    <Text>a</Text>
+  );
+}
+
+export default function Index() {
+  return (
+    <Square>
+    </Square>
+  );
 }
