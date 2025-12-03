fda11636da0304379aa9f7bfd58ec600e1f0c534
eduardomoralesglez <eduardomoralez24@gmail.com>
2025-12-03 16:17:18 +0000

    .

--- PGV/unidad3/tarea1/tarea/src/main/java/unidad3/tarea1/tarea/ejercicio1/BatallaPokemon.java
+++ PGV/unidad3/tarea1/tarea/src/main/java/unidad3/tarea1/tarea/ejercicio1/BatallaPokemon.java
@@ -71,7 +71,7 @@ public class BatallaPokemon {
     }
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(null, );
+        Thread t1 = new Thread();
         Thread t2 = new Thread();
         t1.start();
         t2.start();
