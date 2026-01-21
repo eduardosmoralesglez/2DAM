aa5250b0c5c3f403b70d96ac9abb4172f42d1b0f
eduardomoralesglez <eduardomoralez24@gmail.com>
2026-01-21 15:17:02 +0000

    Tarea repaso DAD Flutter

---
+++ DAD/unidad4/actividades_no_evaluables/actividad_repaso/android/app/src/main/kotlin/com/example/actividad_repaso/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.actividad_repaso
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ DAD/unidad4/actividades_no_evaluables/actividad_repaso/lib/Ejercicio2.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(const App());
+
+/// PROVIDER MÁS SIMPLE POSIBLE
+class MoodModel extends ChangeNotifier {
+  String emoji = '😄';
+
+  void feliz() {
+    emoji = '😄';
+    notifyListeners();
+  }
+
+  void enfadado() {
+    emoji = '😡';
+    notifyListeners();
+  }
+
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MoodModel(),
+      child: const MaterialApp(home: HomePage()),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final model = context.watch<MoodModel>();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mini Provider')),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(model.emoji, style: const TextStyle(fontSize: 80)),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () => context.read<MoodModel>().feliz(),
+              child: const Text('Feliz'),
+            ),
+
+            ElevatedButton(
+              onPressed: () => context.read<MoodModel>().enfadado(),
+              child: const Text('Enfadado'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
